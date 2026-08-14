f02981d2e359ffe09a7185316479872a3c1aeec4
Swathi <swathisudar623@gmail.com>
2026-08-14 13:54:46 +0530

    Fix Render API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://fetch-axios-api-xxxx.onrender.com",
+  baseURL: "https://fetch-axios-mini-project-1.onrender.com",
 });
 
 export default api;
